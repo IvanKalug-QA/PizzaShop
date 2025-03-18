9d3eed24fae43014be5f1270f303e2b2cee0cb91
IVAN-GUY <kaluginivan2002@mail.ru>
2025-03-18 21:27:51 +0300

    add auth backend

--- app/core/db.py
+++ app/core/db.py
@@ -19,3 +19,8 @@ engine = create_async_engine(setting.database_url)
 async_session = AsyncSession(engine)
 
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
+
+
+async def get_async_session():
+    async with AsyncSessionLocal() as async_session:
+        yield async_session
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
 from app.core.config import setting
+from app.api.router import main_router
 
 
 app = FastAPI(title=setting.app_title)
+
+app.include_router(main_router)
